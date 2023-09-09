99e880d20937f2fda531105f43055aeee9cfa1b0
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-09-10 01:52:18 +0300

    Auth

--- FoodiApp/FoodiApp/Models/DTOs/RegisterUserDto.cs
+++ FoodiApp/FoodiApp/Models/DTOs/RegisterUserDto.cs
@@ -1,13 +1,25 @@
-﻿namespace FoodiApp.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodiApp.Models.DTOs
 {
 	public class RegisterUserDto
 	{
-		public string UserName { get; set; }
+		[Required]
+		public string Name { get; set; }
 
-		public string Email { get; set; }	
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
 
-		public string Password { get; set; }	
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password), ErrorMessage = "Passords Are Not Identical")]
+		public string ConfirmPassword { get; set; }
+		[Required]
 		public string Phone { get; set; }
 	}
 }
