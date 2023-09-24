b9bdf0890e2f70f8d72d941cb17db5dbd2a76406
Ala' Abusalem <23037404@student.ltuc.com>
2023-09-24 11:38:34 +0300

    sending welcome email to the client

---
+++ FoodiApp/FoodiApp/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using FoodiApp.Models.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FoodiApp.Controllers
+{
+	public class OrdersController : Controller
+	{
+		private readonly IEmail emailService;
+        public OrdersController(IEmail emailservice)
+        {
+            emailService= emailservice;	
+        }
+        [Authorize(Roles = "Client")]
+
+        public IActionResult Index()
+		{
+			var claimsIdentity = User.Identity as ClaimsIdentity;
+			if (claimsIdentity != null)
+			{
+				var emailClaim = claimsIdentity.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+				if (emailClaim != null)
+				{
+					var emailValue = emailClaim.Value;
+					string welcomeClient = $"thank you {User.Identity.Name} for your order from our website QuickBites";
+					string subject = "Welcome";
+					emailService.SendEmailAsync(emailValue, subject, welcomeClient);
+					// Use the emailValue as needed
+				}
+			}
+            return RedirectToAction("Index", "ShoppingCarts");
+        }
+    }
+}
